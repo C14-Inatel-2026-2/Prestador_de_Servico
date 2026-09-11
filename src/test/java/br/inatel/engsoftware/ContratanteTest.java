d4faf29ea27f63dda016b93e0d288c260a4f2ccb
Laylateles <135770455+Laylateles@users.noreply.github.com>
2026-09-11 08:28:34 -0300

    Acrescentei mais um teste para a classe contratante

--- src/test/java/br/inatel/engsoftware/ContratanteTest.java
+++ src/test/java/br/inatel/engsoftware/ContratanteTest.java
@@ -21,15 +21,32 @@ public class ContratanteTest {
         prestador = new Prestador(8,"Paulo Ricardo Alvez","Paulo@gmail.com","8756985",
                 "95555-5555", criptografiaMock, "Carpinteiro","Faço carpintaria a mais de 25 anos, sou o senhor responsavel e trabalhador, caso precisem dos meus serviços estou a disposição!",
                 "Cachoeira de Minas",34.8);
+
+        contratante = new Contratante(2,"Roberto Carlos da Siva","roberto@gmail.com","1234567",
+                "98888-8888",criptografiaMock,"Santa Rita do Sapucai");
     }
 
     @Test
     void testeReceberNotaInvalidaAvaliacao(){
         criptografiaMock = Mockito.mock(CriptografiaService.class);
-        contratante = new Contratante(2,"Roberto Carlos da Siva","roberto@gmail.com","1234567", "98888-8888",criptografiaMock,"Santa Rita do Sapucai");
 
         Avaliacao notaInvalida = contratante.avaliarUsuario(prestador,-1,"Muito bom");
         Assertions.assertNull(notaInvalida); //como a nota não é valida deve retornar null
+    }
+
+    @Test
+    void testeAvaliarUsuarioComMesmoId(){
+        criptografiaMock = Mockito.mock(CriptografiaService.class);
+
+        prestador = new Prestador(2,"Paulo Ricardo Alvez","Paulo@gmail.com","8756985",
+                "95555-5555", criptografiaMock, "Carpinteiro","Faço carpintaria a mais de 25 anos, sou o senhor responsavel e trabalhador, caso precisem dos meus serviços estou a disposição!",
+                "Cachoeira de Minas",34.8);
+
+        contratante = new Contratante(2,"Roberto Carlos da Siva","roberto@gmail.com","1234567",
+                "98888-8888",criptografiaMock,"Santa Rita do Sapucai");
+
+        Avaliacao idIgual = contratante.avaliarUsuario(prestador,5,"Muito bom");
+        Assertions.assertNull(idIgual); //como estou colocando o id do avaliador igual o id de quem ele esta avaliando
     }
 }
 
